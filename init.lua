6f2a9c889bd2931e89efe5d539dc22d102ef9636
Julian Ruess <julianr@linux.ibm.com>
2023-03-24 16:48:37 +0100

    Speed up unnamedplus

--- init.lua
+++ init.lua
@@ -144,18 +144,18 @@ require('gitsigns').setup()
 vim.cmd('set shell=/usr/bin/zsh')
 vim.cmd('colorscheme gruvbox')
 vim.g.clipboard = {
- name = "xsel",
- copy = {
-  ["+"] = "xsel --nodetach -i -b",
-  ["*"] = "xsel --nodetach -i -p",
- },
- paste = {
-  ["+"] = "xsel  -o -b",
-  ["*"] = "xsel  -o -b",
- },
- cache_enabled = 1,
+	name = "xsel",
+	copy = {
+		["+"] = "xsel -i -b",
+		["*"] = "xsel -i -p",
+	},
+	paste = {
+		["+"] = "xsel  -o -b",
+		["*"] = "xsel  -o -b",
+	},
+	cache_enabled = 1,
 }
-vim.cmd('set clipboard+=unnamedplus')
+vim.opt.clipboard = "unnamedplus"
 vim.cmd('set list')
 vim.cmd('set listchars=tab:*\\ ,eol:¬,trail:~')
 vim.wo.number = true
