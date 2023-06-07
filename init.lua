068d5dc81797d641561f3789ae5978eb6d3b3e76
Julian Ruess <julian.ruess@posteo.de>
2023-06-07 22:38:14 +0200

    Add local snippets dir

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ end
 
 -- luasnip (used by nvim-cmp)
 local luasnip = require 'luasnip'
-require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
 -- nvim-cmp setup (better completion than default nvim completion)
 local cmp = require 'cmp'
