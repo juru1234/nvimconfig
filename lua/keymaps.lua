cc071b7b4cc27c36cc43098b938bb06545d9b2e1
Julian Ruess <julianr@linux.ibm.com>
2023-10-20 12:20:02 +0200

    Update to onedark theme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,7 +31,6 @@ map('n', '<leader>c', ':nohl<CR>')
 
 -- Toggle auto-indenting for code paste
 map('n', '<F2>', ':set invpaste paste?<CR>')
-vim.opt.pastetoggle = '<F2>'
 
 -- Change split orientation
 map('n', '<leader>tk', '<C-w>t<C-w>K') -- change vertical to horizontal
