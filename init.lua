80c341dc779b4fe7bf7ce8a262f18ffce051d3c4
Julian Ruess <julianr@linux.ibm.com>
2024-03-05 14:20:32 +0100

    add transparent bg
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- init.lua
+++ init.lua
@@ -23,6 +23,8 @@ require('keymaps')
 
 -------------------------------------------------------------------
 -- Use the gruvbox scheme
+vim.cmd("let g:gruvbox_transparent_bg = 1")
+vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
 vim.cmd('colorscheme gruvbox')
 -------------------------------------------------------------------
 
